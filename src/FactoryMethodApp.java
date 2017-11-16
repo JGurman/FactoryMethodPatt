de989196612121569d1c757b28be5caee858f772
JGurman <pereverzyev2517@gmail.com>
2017-11-16 20:13:49 +0200

    ssdasd

---
+++ src/FactoryMethodApp.java
@@ -0,0 +1,43 @@
+import java.util.Date;
+
+public class FactoryMethodApp {
+
+    public static void main(String[] args) {
+        WatchMaker maker = new DigitalWatchMaker();
+        Watch watch = maker.createWatch();
+        watch.showTime();
+    }
+}
+interface Watch {
+    void showTime();
+}
+
+class DigitalWatch implements Watch {
+    public void showTime() {
+        System.out.println(new Date());
+    }
+}
+
+class RomeWatch implements Watch {
+    public void showTime() {
+        System.out.println("VII-XX");
+    }
+}
+
+interface WatchMaker {
+    Watch createWatch();
+}
+
+class DigitalWatchMaker implements WatchMaker {
+
+    public Watch createWatch() {
+        return new DigitalWatch();
+    }
+}
+
+class RomeWatchMaker implements WatchMaker {
+
+    public Watch createWatch() {
+        return new RomeWatch();
+    }
+}
